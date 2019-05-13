d7dd8103a13e59e154176e7337987f5c436cb0c5
ksn7 <ksn7@students.calvin.edu>
2019-05-13 12:11:00 -0400

    upload working code for findRoute

--- project/findRoute.pl
+++ project/findRoute.pl
@@ -9,10 +9,10 @@ directTrain(stAvold, freyming).
 directTrain(fahlquemont, stAvold).
 directTrain(metz, fahlquemont).
 directTrain(nancy, metz).
-% new stuff - add it as info here b/c 
-%    otherwise the recursive stuff
-%    doesn't work? Maybe should figure
-%    that out
+% new stuff - add it as info here. Not
+% sure why adding a reversing function
+% doesn't work. Ex) solutions time out
+% on directTrain(A,B):- directTrain(B,A).
 directTrain(dudweiler, saarbruecken).
 directTrain(saarbruecken, forbach).
 directTrain(forbach, freyming).
@@ -30,30 +30,34 @@ append([H|T],L2,[H|L3]):- append(T,L2,L3).
 % member list function taken from
 % LPN 4.2, used in calculating route
 % to avoid revisiting cities.
-member(X,[X|T]).
-member(X,[H|T]):- member(X,T).
+member(X,[X|_]).
+member(X,[_|T]):- member(X,T).
 
 % 3.3 Exercise
 travelFromTo(X,Y):- directTrain(X,Y).
 travelFromTo(X,Z):- travelFromTo(X,Y),
     travelFromTo(Y,Z).
 
+
 % new code
-% FIXME: works only in one direction, 
-%    can't figure out how to not
-%    revisit cities
 
 % This cut ensures only one solution
 %    is searched for
-route(X,Y, Route):- directTrain(X,Y),
+route1(X,Y,Visited,Route):- directTrain(X,Y),
+    \+ member(X, Visited),
     Route=[X,Y], !.
-% This cut ensures one solution, but not sure
-% that the member part is working correctly.
-route(X,Z, Route):- directTrain(X,Y),
-    route(Y, Z, RouteTail), 
+% The \+ cut removes solutions where the city has
+% already been visited to prevent cycles.
+% The cut on the end finds only one solution.
+route1(X,Z,Visited,Route):- \+ member(X, Visited),
+    directTrain(X,Y),
+    route1(Y, Z, [X|Visited], RouteTail), 
     append([X], RouteTail, Route),
-    \+ member(X, RouteTail),
     !.
 
+% Adding separate route predicate that calls route1
+% because the Visited argument allows me to remove
+% cycles from graphs.
+route(X,Y,Route):- Visited=[], route1(X,Y,Visited,Route).
 
 
